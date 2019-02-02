7a048330ca544e506326947a5c7ed147228ddfdf
william <billyfeller@gmail.com>
2019-02-02 22:27:41 +0100

    add comments

--- config/parental.php
+++ config/parental.php
@@ -1,9 +1,12 @@
 <?php
 
 return [
+    // The directories that will be analyzed to find models.
     'model_directories' => [
         app_path(),
     ],
 
+    // Please leave the //just an anchor comment. If you're good with regex or know a good way to easily re-write
+    // the discovered children, please submit an issue or PR.
     'discovered_children' => [], //just an anchor
 ];
