8264e897c2f76f2831bd5bebe838033040883a11
zecipriano <ze@zecipriano.com>
2019-02-27 12:05:20 +0000

    Support Laravel 5.8

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
