6fa0a852a2bd82541cfcc509bec831e97439c3c6
Anthony Clark <anthonyclark@gmail.com>
2025-02-06 13:11:58 -0800

    Remove unused import

--- src/HasChildren.php
+++ src/HasChildren.php
@@ -2,7 +2,6 @@
 
 namespace Parental;
 
-use Closure;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
